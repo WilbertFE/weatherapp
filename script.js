93bf1e0734470cdb6514deb8481e7875a8885f9e
WilbertFE <wilbertbernadi699@gmail.com>
2023-12-08 17:58:30 +0700

    Scrollbar done

--- script.js
+++ script.js
@@ -4,12 +4,9 @@ const input = document.querySelector('.input-container input');
 const btn = document.querySelector('.input-container button');
 
 btn.addEventListener('click', async function (){
-    // make variabel cityName
-    const cityName = input.value;
-    input.value = '';
-
     // taking data
-    const data = await getData(cityName);
+    const data = await getData(input.value);
+    input.value = '';
     updateUI(data);
 });
 
@@ -79,69 +76,56 @@ function getDataForecast(cityName){
 }
 
 function updateUI (data) {
-    // taking information that is needed
-    const cityName = data.name;
-    const iconCode = data.weather[0].icon;
-    const main = data.weather[0].main;
-    const description = data.weather[0].description;
-    const temp = data.main.temp;
-    const feelsLike = data.main.feels_like;
-    const humidity = data.main.humidity;
-    const visibility = data.visibility;
-    const pressure = data.main.pressure;
-    const windSpeed = data.wind.speed;
-    const clouds = data.clouds.all;
-
     // update UI
     let gridContainerFragment= '';
     let forecastContainerFragment = '';
     gridContainerFragment = `
     <div class="icon-container">
-                <h1>${cityName}</h1>
+                <h1>${data.name}</h1>
                 <div class="image-container"></div>
-                <h2>${main}</h2>
-                <p>${description}</p>
+                <h2>${data.weather[0].main}</h2>
+                <p>${data.weather[0].description}</p>
             </div>
             <div class="information-container">
                 <div class="information">
                     <h2>Suhu</h2>
-                    <h1>${Math.round(temp)}°C</h1>
-                    <p>terasa seperti ${Math.round(feelsLike)}°C</p>
+                    <h1>${Math.round(data.main.temp)}°C</h1>
+                    <p>terasa seperti ${Math.round(data.main.feels_like)}°C</p>
                 </div>
                 <div class="information">
                     <h2>Kelembapan Air</h2>
-                    <h1>${humidity}</h1>
+                    <h1>${data.main.humidity}</h1>
                     <p>persen(%)</p>
                 </div>
                 <div class="information">
                     <h2>Jarak Pandang</h2>
-                    <h1>${Math.round(visibility/1000)}</h1>
+                    <h1>${Math.round(data.visibility/1000)}</h1>
                     <p>km</p>
                 </div>
                 <div class="information">
                     <h2>Tekanan Udara</h2>
-                    <h1>${pressure}</h1>
+                    <h1>${data.main.pressure}</h1>
                     <p>hPa</p>
                 </div>
                 <div class="information">
                     <h2>Kecepatan Angin</h2>
-                    <h1>${Math.round(windSpeed)}</h1>
+                    <h1>${Math.round(data.wind.speed)}</h1>
                     <p>km/j</p>
                 </div>
                 <div class="information">
                     <h2>Awan</h2>
-                    <h1>${clouds}%</h1>
+                    <h1>${data.clouds.all}%</h1>
                     <p>menutupi langit</p>
                 </div>
             </div>`; 
-        forecastContainerFragment += `<h2 class="forecast" data-city="${cityName}">lihat ramalan cuaca?</h2>
-        <p class="forecast" data-city="${cityName}">ramalan harian dan mingguan tersedia</p>`;
+        forecastContainerFragment += `<h2 class="forecast" data-city="${data.name}">lihat ramalan cuaca?</h2>
+        <p class="forecast" data-city="${data.name}">ramalan harian dan mingguan tersedia</p>`;
         // append fragment to gridContainer and change the display into grid, change the icon
         const gridContainer = document.querySelector('.grid-container');
         gridContainer.style.display = 'grid';
         gridContainer.innerHTML = gridContainerFragment;
         const icon = document.querySelector('.image-container');
-        icon.style.backgroundImage = `url(https://openweathermap.org/img/wn/${iconCode}@2x.png)`;
+        icon.style.backgroundImage = `url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)`;
         // append fragment to forecast container
         const forecastContainer = document.querySelector('.forecast-container');
         forecastContainer.innerHTML = forecastContainerFragment;
