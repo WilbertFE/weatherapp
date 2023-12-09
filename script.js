d5f14f30fa99cef1b3ac9f89de156fa3bad54c9e
WilbertFE <wilbertbernadi699@gmail.com>
2023-12-09 13:16:05 +0700

    Responsive done

--- script.js
+++ script.js
@@ -28,11 +28,22 @@ document.addEventListener('click', async function(e){
     }
 });
 
-
-
-
-
-
+function getData (cityName) {
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&lang=id&units=metric&mode=json&appid=736d300233c8e86a886feabe3ef4b69a`)
+        .then(response => {
+            if (!response.ok){
+                if (response.statusText === 'Bad Request'){
+                    throw new Error('Tolong Isi Nama Kota Dengan Benar!');
+                } else if (response.statusText === 'Not Found'){
+                    throw new Error('Kota Tidak Ditemukan');
+                } else {
+                    throw new Error(response.statusText + ' (ada kesalahan pada sistem)');
+                }
+            }
+            return response.json();
+        })
+        .then(response => response);
+}
 
 function getDataForecast(cityName){
     return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&lang=id&units=metric&mode=json&appid=736d300233c8e86a886feabe3ef4b69a&cnt=40`)
@@ -44,69 +55,6 @@ function getDataForecast(cityName){
         })
         .then(response => response);
 }
-
-
-function getData (cityName) {
-    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&lang=id&units=metric&mode=json&appid=736d300233c8e86a886feabe3ef4b69a`)
-        .then(response => {
-            if (!response.ok){
-                if (response.statusText === 'Bad Request'){
-                    throw new Error('Tolong Isi Nama Kota Dengan Benar!');
-                } else if (response.statusText === 'Not Found'){
-                    throw new Error('Data Tidak Ditemukan');
-                } else {
-                    throw new Error(response.statusText + ' (ada kesalahan pada sistem)');
-                }
-            }
-            return response.json();
-        })
-        .then(response => response);
-}
-
-
-function updateUIForecastWeekly(data){
-    let fragment = '';
-    for (let i=0; i<data.list.length; i++){
-        if (data.list[i].dt_txt.includes('12:00')){
-            fragment += `
-            <div class="twelve-forecast">
-                <h4>${data.list[i].dt_txt}</h4>
-                <h4>${data.city.name}</h4>
-                <img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png" alt="weather.png">
-                <h3>${Math.round(data.list[i].main.feels_like)}°C</h3>
-                <p>${data.list[i].weather[0].description}</p>
-            </div>
-            `;
-        }
-    }
-    // append element
-    const container = document.querySelector('.weekly-forecast');
-    container.innerHTML = fragment;
-}
-
-
-
-function updateUIForecast(data){
-    let dailyForecastFragments = '';
-    let dailyForecastFragment = '';
-    for (let i=0; i<8; i++){
-        dailyForecastFragment += `
-        <div class="hourly-forecast">
-            <h4>${data.list[i].dt_txt}</h4>
-            <h4>${data.city.name}</h4>
-            <img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png" alt="weather.png">
-            <h3>${Math.round(data.list[i].main.feels_like)}°C</h3>
-            <p>${data.list[i].weather[0].description}</p>
-        </div>
-        `;
-    }
-    dailyForecastFragments = `<h2 class="daily-forecast-text">ramalan harian (24 jam)</h2> <div class="daily-forecast">${dailyForecastFragment}</div> <h2 class="weekly-forecast-text">ramalan mingguan (setiap jam 12)</h2> <div class="weekly-forecast"></div>`;
-    // append element
-    const forecastContainer = document.querySelector('.forecast-container');
-    forecastContainer.classList.add('fContainer');
-    forecastContainer.innerHTML = dailyForecastFragments;
-}
-
 
 function updateUI (data) {
     // update UI
@@ -155,7 +103,7 @@ function updateUI (data) {
         <p class="forecast" data-city="${data.name}">ramalan harian dan mingguan tersedia</p>`;
         // append fragment to gridContainer and change the display into grid, change the icon
         const gridContainer = document.querySelector('.grid-container');
-        gridContainer.style.display = 'grid';
+        gridContainer.classList.add('gContainer');
         gridContainer.innerHTML = gridContainerFragment;
         const icon = document.querySelector('.image-container');
         icon.style.backgroundImage = `url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)`;
@@ -166,3 +114,43 @@ function updateUI (data) {
         forecastContainer.style.display = 'block';
 }
 
+function updateUIForecast(data){
+    let dailyForecastFragments = '';
+    let dailyForecastFragment = '';
+    for (let i=0; i<9; i++){
+        dailyForecastFragment += `
+        <div class="hourly-forecast">
+            <h4>${data.list[i].dt_txt}</h4>
+            <h4>${data.city.name}</h4>
+            <img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png" alt="weather.png">
+            <h3>${Math.round(data.list[i].main.feels_like)}°C</h3>
+            <p>${data.list[i].weather[0].description}</p>
+        </div>
+        `;
+    }
+    dailyForecastFragments = `<h2 class="daily-forecast-text">ramalan harian (24 jam)</h2> <div class="daily-forecast">${dailyForecastFragment}</div> <h2 class="weekly-forecast-text">ramalan mingguan (setiap jam 12)</h2> <div class="weekly-forecast"></div>`;
+    // append element
+    const forecastContainer = document.querySelector('.forecast-container');
+    forecastContainer.classList.add('fContainer');
+    forecastContainer.innerHTML = dailyForecastFragments;
+}
+
+function updateUIForecastWeekly(data){
+    let fragment = '';
+    for (let i=0; i<data.list.length; i++){
+        if (data.list[i].dt_txt.includes('12:00')){
+            fragment += `
+            <div class="twelve-forecast">
+                <h4>${data.list[i].dt_txt}</h4>
+                <h4>${data.city.name}</h4>
+                <img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png" alt="weather.png">
+                <h3>${Math.round(data.list[i].main.feels_like)}°C</h3>
+                <p>${data.list[i].weather[0].description}</p>
+            </div>
+            `;
+        }
+    }
+    // append element
+    const container = document.querySelector('.weekly-forecast');
+    container.innerHTML = fragment;
+}
